fde8b74f156fed6c74f20aa7a2df961f0cda4868
alexandr <as.grom@gmail.com>
2020-05-08 19:37:43 +0300

    done

--- app/server.py
+++ app/server.py
@@ -21,10 +21,18 @@ class ClientProtocol(asyncio.Protocol):
         if self.login is None:
             # login:User
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace("\r\n", "")
+                login = decoded.replace("login:", "").strip()
+
+                # вызываем метод проверки логина
+                if not self.check_login(login):
+                    return
+
                 self.transport.write(
-                    f"Привет, {self.login}!".encode()
+                    f"Привет, {self.login}!\n".encode()
                 )
+
+                # посылаем историю чата
+                self.send_history()
         else:
             self.send_message(decoded)
 
@@ -36,6 +44,9 @@ class ClientProtocol(asyncio.Protocol):
             if client.login != self.login:
                 client.transport.write(encoded)
 
+        # добавляем сообщение в историю
+        self.server.add_msg_to_history(format_string)
+
     def connection_made(self, transport: transports.Transport):
         self.transport = transport
         self.server.clients.append(self)
@@ -45,15 +56,57 @@ class ClientProtocol(asyncio.Protocol):
         self.server.clients.remove(self)
         print("Соединение разорвано")
 
+    def check_login(self, login):
+        """Проверка логина
+
+        Если логин существует, закрываем соединение с пользователем.
+        """
+        for client in self.server.clients:
+            if login == client.login:
+                self.transport.write('Логин {{{0}}} занят, попробуйте другой.'.format(login).encode())
+                self.transport.close()
+                return False
+        self.login = login
+        return True
+
+    def send_history(self, count=10):
+        """Посылает историю чата пользователю
+
+        Количество сообщений чата задается аргументом count(по-умолчанию 10)
+        """
+        len_history = len(self.server.history)
+
+        if len_history > count:
+            messages = '\n'.join(self.server.history[len_history - count:])
+        else:
+            messages = '\n'.join(self.server.history[:])
+
+        messages = 'Последние сообщения в чате:\n' + messages
+        self.transport.write(messages.encode())
+
 
 class Server:
     clients: list
+    history: list
 
     def __init__(self):
         self.clients = []
+        self.history = []
+
+        # макс. кол-во сообщений в истории
+        self.max_history = 20
 
     def create_protocol(self):
         return ClientProtocol(self)
+
+    def add_msg_to_history(self, msg):
+        """Добавлет сообщение в конец истории
+
+        Если история достигла максимума, удаляется первое сообщение.
+        """
+        if len(self.history) > self.max_history:
+            self.history.pop(0)
+        self.history.append(msg)
 
     async def start(self):
         loop = asyncio.get_running_loop()
